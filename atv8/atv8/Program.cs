12b482fdfa3d9d0d98921d5faeb4fe27b8cf52e3
Pietra Baiocco <ddspietra@gmail.com>
2025-09-10 11:03:01 -0300

    atv8

---
+++ atv8/atv8/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("Digite um número, exeto 0.");
+num = int.Parse(Console.ReadLine());
+if (num > 0)
+	Console.WriteLine($"O número {num} é positivo.");
+else if (num == 0)
+	Console.WriteLine($"O número {num} é inválido.");
+else
+	Console.WriteLine($"O número {num} é negativo.");
