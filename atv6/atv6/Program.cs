122ea85d5d5d1fd50f48d2b1c2708232fef26897
Pietra Baiocco <ddspietra@gmail.com>
2025-09-10 10:43:45 -0300

    atv6

---
+++ atv6/atv6/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+double salario;
+string nome;
+double novo;
+Console.WriteLine("Digite seu nome.");
+nome = Console.ReadLine();
+Console.WriteLine($"Olá, {nome}! Digite seu salário para calcular o reajuste.");
+salario = double.Parse(Console.ReadLine());
+novo = salario + (salario * 0.10);
+Console.WriteLine($"Seu novo salário, com reajuste de 15%, é no valor de ${novo}.");
