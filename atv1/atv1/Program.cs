0e538d569d66a028f62d944f1c321e31d2b1cc45
Pietra Baiocco <ddspietra@gmail.com>
2025-09-10 09:15:23 -0300

    atv1

---
+++ atv1/atv1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a = 10;
+int b = 20;
+int aux;
+Console.WriteLine($"A variável 'A' armazena o valor {a}, enquanto a variável 'B' armazena o valor de {b}.");
+aux = a;
+a = b;
+b = aux;
+Console.WriteLine($"A variável 'A' armazena o valor {a}, enquanto a variável 'B' armazena o valor de {b}.");
