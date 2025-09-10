7c927766e25d156cabe7d8c6ef6c075d471f66cd
Pietra Baiocco <ddspietra@gmail.com>
2025-09-10 09:32:13 -0300

    atv4

---
+++ atv4/atv4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int ano;
+int mes;
+int dia;
+int idade;
+Console.WriteLine("Quantos anos você tem?");
+ano = int.Parse(Console.ReadLine());
+Console.WriteLine("Quantos meses se passaram do seu aniversário?");
+mes = int.Parse(Console.ReadLine());
+Console.WriteLine("Quantos dias se passaram da data do seu aniversário?");
+dia = int.Parse(Console.ReadLine());
+idade = (ano * 365) + (mes * 30) + dia;
+Console.WriteLine($"Sua idade expressa em dias equivale a {idade} dias.");
