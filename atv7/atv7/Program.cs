9ac3c9f1a5e6332064552b39d129911a596331c1
Pietra Baiocco <ddspietra@gmail.com>
2025-09-10 10:55:43 -0300

    atv7

---
+++ atv7/atv7/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("Digite um número.");
+num = int.Parse(Console.ReadLine());
+if (num > 10)
+	Console.WriteLine("Esse número é maior que 10.");
+else if (num < 10)
+	Console.WriteLine("Esse número é menor que 10.");
