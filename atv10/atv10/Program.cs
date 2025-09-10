a2945e993efb6308e50a857a9016a774076ab3ae
Pietra Baiocco <ddspietra@gmail.com>
2025-09-10 11:20:48 -0300

    atv10

---
+++ atv10/atv10/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+
+int n1;
+int n2;
+Console.WriteLine("Digite a nota da primeira prova.");
+n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a nota da segunda prova.");
+n2 = int.Parse(Console.ReadLine());
+double media = (n1 + n2) / 2;
+if (media >= 6)
+{
+	Console.WriteLine($"Sua média foi de {media}, você foi aprovado!");
+}
+else
+{
+	Console.WriteLine($"Sua média foi de {media}, você foi reprovado.");
+}
