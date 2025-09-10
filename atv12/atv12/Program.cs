6779e7cc7fe6e7617a21cf2738885a2bdd3e9e55
Pietra Baiocco <ddspietra@gmail.com>
2025-09-10 11:39:49 -0300

    atv12

---
+++ atv12/atv12/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int v1;
+int v2;
+Console.WriteLine("Digite um número.");
+v1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite um número diferente.");
+v2 = int.Parse(Console.ReadLine());
+if (v1 > v2)
+{
+	Console.WriteLine($"O maior número é {v1}.");
+}
+else
+{
+	Console.WriteLine($"O maior número é {v2}.");
+}
