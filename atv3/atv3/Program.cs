98e1dd9a64f68769a0d019c57cae12447b1a13c5
Pietra Baiocco <ddspietra@gmail.com>
2025-09-10 09:26:45 -0300

    atv3

---
+++ atv3/atv3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int largura;
+int altura;
+int area;
+Console.WriteLine("Digite o valor da largura do quadrilátero.");
+largura = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite o valor da altura do quadrilátero.");
+altura = int.Parse(Console.ReadLine());
+area = largura * altura;
+Console.WriteLine($"O valor da área do quadrilátero é de {area}.");
