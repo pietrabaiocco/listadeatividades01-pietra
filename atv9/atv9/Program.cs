a6bba129c32a7a3ff53525797087f40a7ecc7722
Pietra Baiocco <ddspietra@gmail.com>
2025-09-10 11:15:01 -0300

    atv9

---
+++ atv9/atv9/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+int quant;
+double valor;
+Console.WriteLine("--- MAÇÃS DO BOSQUE ---");
+Console.WriteLine("Digite a quantidade de maçãs adquiridas.");
+quant = int.Parse(Console.ReadLine());
+if (quant < 12)
+{
+	valor = quant * 1.30;
+	Console.WriteLine($"A compra de {quant} maçãs resultou em ${valor}.");
+}
+else
+{
+	valor = quant * 1;
+	Console.WriteLine($"A compra de {quant} maçãs resultou em ${valor}.");
+}
