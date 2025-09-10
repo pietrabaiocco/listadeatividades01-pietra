1c172eec92415cf21a100fc71cf295d1f83c4035
Pietra Baiocco <ddspietra@gmail.com>
2025-09-10 09:20:15 -0300

    atv2

---
+++ atv2/atv2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num;
+Console.WriteLine("Digite um número.");
+num = int.Parse(Console.ReadLine());
+num -= 1;
+Console.WriteLine($"O seu antecessor é {num}.");
