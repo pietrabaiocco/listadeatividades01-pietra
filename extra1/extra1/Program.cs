64404b786099b6953b17685bb458a0c17e2743cd
pietra <ddspietra@gmail.com>
2025-09-16 07:55:08 -0300

    extra1

---
+++ extra1/extra1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int real;
+double dolar;
+Console.WriteLine("Digite a quantidade de dinheiro em R$.");
+real = int.Parse(Console.ReadLine());
+dolar = real / 3.45;
+Console.WriteLine($"Você tem US${dolar}.");
