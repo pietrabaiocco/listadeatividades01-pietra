a7628fd65b8463001ebc4d696fad4cfef30fa1df
pietra <ddspietra@gmail.com>
2025-09-16 08:12:09 -0300

    extra2

---
+++ extra2/extra2/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int largura, altura;
+Console.WriteLine("Digite a largura da parede a ser pintada.");
+largura = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite a altura da parede a ser pintada.");
+altura = int.Parse(Console.ReadLine());
+double area = largura * altura;
+double tinta = area / 2;
+Console.WriteLine($"Você precisará de {tinta} latas de 2l de tinta para pintar {area}m² de parede.");
