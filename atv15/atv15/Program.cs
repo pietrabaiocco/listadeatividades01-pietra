2f68137137b198887b1138945d55c6b8cc088e25
pietra <ddspietra@gmail.com>
2025-09-16 09:38:45 -0300

    atv15

---
+++ atv15/atv15/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Números entre 1000 e 2000 que deixam resto 5 ao serem divididos por 11:");
+for (int numero = 1000; numero <= 2000; numero++)
+{
+    if (numero % 11 == 5)
+    {
+        Console.WriteLine(numero);
+    }
+}
