be823ca2c25266a5630144560a8af608dac4101a
pietra <ddspietra@gmail.com>
2025-09-16 09:45:47 -0300

    atv16

---
+++ atv16/atv16/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite um número inteiro: ");
+string entrada = Console.ReadLine();
+
+if (int.TryParse(entrada, out int numero))
+{
+    if (numero < 0)
+    {
+        Console.WriteLine("Fatorial de número negativo não existe.");
+    }
+    else
+    {
+        long fatorial = 1;
+
+        for (int i = 1; i <= numero; i++)
+        {
+            fatorial *= i;
+        }
+
+        Console.WriteLine($"{numero}! = {fatorial}");
+    }
+}
+else
+{
+    Console.WriteLine("Entrada inválida. Por favor, digite um número inteiro.");
+}
