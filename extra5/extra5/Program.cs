8545d7f43ded1dad6ba4d23e6dd413e626066996
pietra <ddspietra@gmail.com>
2025-09-16 09:21:15 -0300

    extra5

---
+++ extra5/extra5/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int salario;
+Console.WriteLine("Digite o valor do seu salário para calcular 15% de aumento.");
+salario = int.Parse(Console.ReadLine());
+double aumento = salario * 1.15;
+Console.WriteLine($"Seu novo salário é no valor de R${aumento}");
