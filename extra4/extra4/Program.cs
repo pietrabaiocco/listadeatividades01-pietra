c5fdce19862d9776b5e7b7fe807bfd17cda117a3
pietra <ddspietra@gmail.com>
2025-09-16 09:07:24 -0300

    extra4

---
+++ extra4/extra4/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int prod;
+Console.WriteLine("Digite o valor do produto que deseja saber o valor promocional.");
+prod = int.Parse(Console.ReadLine());
+double valor = prod - (prod * 0.05);
+Console.WriteLine($"O valor promocional é de R${valor}.");
