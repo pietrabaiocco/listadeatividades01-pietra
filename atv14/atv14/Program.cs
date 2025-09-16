5b4f265288a0c77d6954ae7fc66ef152ac1aa34a
pietra <ddspietra@gmail.com>
2025-09-16 09:32:21 -0300

    atv14

---
+++ atv14/atv14/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num, contador;
+Console.WriteLine("Digite um número maior que zero.");
+num = int.Parse(Console.ReadLine());
+contador = 0;
+while (contador <= num)
+{
+    Console.WriteLine(contador);
+    contador++;
+}
